524895caef6580f48c9d6e16b63e9559ed85ed6a
Mario J. Schwaiger <mario.schwaiger@v2c2.at>
2026-01-14 22:22:01 +0100

    Working but with weird issues

---
+++ SolveAsi1.py
@@ -0,0 +1,36 @@
+import json 
+import yaml 
+import re 
+import sys
+
+BITCNT=8
+
+# Source - https://stackoverflow.com/a/36394050
+# Posted by Joe T. Boka
+# Retrieved 2026-01-14, License - CC BY-SA 3.0
+
+def split_string(string, split_string):
+    return [string[i:i+split_string] for i in range(0, len(string), split_string)]
+
+def doEncode(message, praeambel="10101011"):
+    if len(message)%BITCNT!=0 :
+        print(str(len(message))+" %"+str(BITCNT)+" = "+str(len(message)%BITCNT))
+        return 0
+
+    message=message.split(praeambel,1)[1]
+    
+    blocks = split_string(message,BITCNT)
+    
+    decode = []
+    for block in blocks:
+        dec = int(block, 2)        
+        decode.append(chr(dec))
+
+    print(''.join(decode))
+
+
+if __name__ == "__main__":
+    doEncode(sys.argv[1])
+    
+
+    
