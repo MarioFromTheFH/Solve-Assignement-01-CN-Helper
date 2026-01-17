d5be850cdfe92d5a5bb6eddef088ba5f64026295
Mario J. Schwaiger <mario.schwaiger@v2c2.at>
2026-01-17 23:08:03 +0100

    Don't use this commit. This is just a big pile of wrong

--- SolveAsi1.py
+++ SolveAsi1.py
@@ -12,22 +12,32 @@ BITCNT=8
 def split_string(string, split_string):
     return [string[i:i+split_string] for i in range(0, len(string), split_string)]
 
-def doEncode(message, offset=8, praeambel="10101011"):
-    if len(message)%BITCNT!=0 :
-        print(str(len(message))+" %"+str(BITCNT)+" = "+str(len(message)%BITCNT))
-        return 0
+def doEncode(message, offset=8*8, praeambel="10101011"):
 
+    print("Removing the first %d characters" % offset)
+
+    str_prae = message[:offset]
     message=message[offset:]
+
+    print(str_prae+": "+str(len(str_prae))+" Zeichen")
+    print(message)
 
     print("The sequence "+praeambel+" appears in the input-string: "+str(message.count(praeambel))+" times")
 
     message=message.split(praeambel,1)[1]
+
+    print("Message after Präambel:")
+    print(message)
+    if len(message)%BITCNT!=0 :
+        print(str(len(message))+" %"+str(BITCNT)+" = "+str(len(message)%BITCNT))
+        return 0    
     
     blocks = split_string(message,BITCNT)
     
     decode = []
     for block in blocks:
-        dec = int(block, 2)        
+        dec = int(block, 2)     
+        print(block+": "+chr(dec))   
         decode.append(chr(dec))
 
     print(''.join(decode))
